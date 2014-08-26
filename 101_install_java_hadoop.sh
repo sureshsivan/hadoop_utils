09e2a2b328dab655f0cf68ff0fc1652e43d6c8ac
Suresh Sivanantham <suresh.sivanantham@gmail.com>
2014-08-26 20:18:05 +0530

    mapred-site.xml sed commands updated

--- 101_install_java_hadoop.sh
+++ 101_install_java_hadoop.sh
@@ -85,7 +85,7 @@ chmod -R 744 .
 #SEDing hadoop xml configs
 # core-site.xml
 #   fs.default.name
-sed -i '/fs.default.name/{n;s/.*/\t<value>hdfs:\/\/localhost:8020<\/value>/;}' core-site.xml
+sed -i '/fs.default.name/{n;s/.*/\t<value>hdfs:\/\/master1:8020<\/value>/;}' core-site.xml
 #   fs.checkpoint.dir
 sed -i '/fs.checkpoint.dir/{n;s/.*/\t<value>\/bigdata\/data\/fs.checkpoint<\/value>/;}' core-site.xml
 #   hadoop.tmp.dir
@@ -97,15 +97,18 @@ sed -i '/dfs.name.dir/{n;s/.*/\t<value>\/bigdata\/data\/dfs.name<\/value>/;}' hd
 #   dfs.data.dir
 sed -i '/dfs.data.dir/{n;s/.*/\t<value>\/bigdata\/data\/dfs.data<\/value>/;}' hdfs-site.xml
 #   dfs.http.address
-sed -i '/dfs.http.address/{n;s/.*/\t<value>localhost:50070<\/value>/;}' hdfs-site.xml
+sed -i '/dfs.http.address/{n;s/.*/\t<value>master2:50070<\/value>/;}' hdfs-site.xml
 #   dfs.secondary.http.address
-sed -i '/dfs.secondary.http.address/{n;s/.*/\t<value>localhost:50090<\/value>/;}' hdfs-site.xml
+sed -i '/dfs.secondary.http.address/{n;s/.*/\t<value>master2:50090<\/value>/;}' hdfs-site.xml
 #   dfs.https.address
-sed -i '/dfs.https.address/{n;s/.*/\t<value>localhost:50470<\/value>/;}' hdfs-site.xml
+sed -i '/dfs.https.address/{n;s/.*/\t<value>master2:50470<\/value>/;}' hdfs-site.xml
 
 # mapred-site.xml
 #   mapreduce.jobhistory.address
+sed -i 'mapreduce.jobhistory.address/{n;s/.*/\t<value>master2:10020<\/value>/;}' mapred-site.xml
 #   mapreduce.jobhistory.webapp.address
+sed -i 'mapreduce.jobhistory.webapp.address/{n;s/.*/\t<value>master2:19888<\/value>/;}' mapred-site.xml
+
     
 # yarn-site.xml
 #   yarn.nodemanager.local-dirs
