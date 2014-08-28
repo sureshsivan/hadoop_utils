ad83625f2c605699f524f0160384a220c68ca05a
Suresh Sivanantham <suresh.sivanantham@gmail.com>
2014-08-28 22:18:11 +0530

    Update 103_setup_additional_nodes.sh

--- 103_setup_additional_nodes.sh
+++ 103_setup_additional_nodes.sh
@@ -1,7 +1,17 @@
 ###########################################################################################
 # Script for setting up additional nodes apart from main node
 ###########################################################################################
-# Assumtion is Hadoo is already configured ans setup.
+# Assumption is Hadoo is already configured ans setup.
+
+# To add secondary namenode configuration in conf/master
+cat master2 > $HADOOP_CONF_DIR/masters
+
+# To Add data nodes into slaves file
+cat slave1 > $HADOOP_CONF_DIR/slaves
+cat slave2 >> $HADOOP_CONF_DIR/slaves
+cat slave3 >> $HADOOP_CONF_DIR/slaves
+
+
 # To set the hostname
 cat <host_name> > /etc/hostname
 
@@ -9,8 +19,6 @@ cat <host_name> > /etc/hostname
 login as root
 su root
 grep -q "^master1" /etc/hosts && sed "s/^master1=.*/master1 <master_1_ip>/" -i file || sed "$ a\master1 <master_1_ip>" -i /etc/hosts
-
-
 grep -q "^master2" /etc/hosts && sed "s/^master2=.*/master1 <master_2_ip>/" -i file || sed "$ a\master2 <master_2_ip>" -i /etc/hosts
 grep -q "^master3" /etc/hosts && sed "s/^master3=.*/master1 <master_3_ip>/" -i file || sed "$ a\master3 <master_3_ip>" -i /etc/hosts
 grep -q "^slave1" /etc/hosts && sed "s/^slave1=.*/slave1 <slave_1_ip>/" -i file || sed "$ a\slave1 <slave_1_ip>" -i /etc/hosts
@@ -21,8 +29,14 @@ grep -q "^slave3" /etc/hosts && sed "s/^slave3=.*/slave1 <slave_3_ip>/" -i file 
 ###########################################################################################
 # Secondary Namenode  / Other services
 ###########################################################################################
-
+# Set up evrything mentioned in revious file..
+# rsync conf from namenode
 
 ###########################################################################################
 # data Nodes  / Yarn Task Nodes
 ###########################################################################################
+# Set up evrything mentioned in revious file..
+# rsync conf from namenode
+
+
+bin/hadoop-daemon.sh start [namenode | secondarynamenode | datanode | jobtracker | tasktracker]
