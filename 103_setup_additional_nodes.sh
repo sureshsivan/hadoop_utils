41de146153cff15a8ada9864c6e618bac7ede211
Suresh Sivanantham <suresh.sivanantham@gmail.com>
2014-08-29 12:02:17 +0530

    Setting up secondary namenode

--- 103_setup_additional_nodes.sh
+++ 103_setup_additional_nodes.sh
@@ -1,8 +1,13 @@
 ###########################################################################################
+###########################################################################################
 # Script for setting up additional nodes apart from main node
 ###########################################################################################
-# Assumption is Hadoop is already configured ans setup.
+###########################################################################################
 
+###########################################################################################
+# Script for setting up additional nodes apart from main node
+###########################################################################################
+# Assumption is Hadoop is already configured ans setup(https://github.com/v8-suresh/hadoop_utils/blob/master/101_install_java_hadoop.sh)
 # To add secondary namenode configuration in conf/master
 cat master2 > $HADOOP_CONF_DIR/masters
 
@@ -11,26 +16,31 @@ cat slave1 > $HADOOP_CONF_DIR/slaves
 cat slave2 >> $HADOOP_CONF_DIR/slaves
 cat slave3 >> $HADOOP_CONF_DIR/slaves
 
-
 # To set the hostname
-cat <host_name> > /etc/hostname
+cat master1 > /etc/hostname
 
-# To sync up hosts file in all the machines
-login as root
+# Setup DNS to hosts file - in namenode
+#login as root
 su root
-grep -q "^master1" /etc/hosts && sed "s/^master1=.*/master1 <master_1_ip>/" -i file || sed "$ a\master1 <master_1_ip>" -i /etc/hosts
-grep -q "^master2" /etc/hosts && sed "s/^master2=.*/master1 <master_2_ip>/" -i file || sed "$ a\master2 <master_2_ip>" -i /etc/hosts
-grep -q "^master3" /etc/hosts && sed "s/^master3=.*/master1 <master_3_ip>/" -i file || sed "$ a\master3 <master_3_ip>" -i /etc/hosts
-grep -q "^slave1" /etc/hosts && sed "s/^slave1=.*/slave1 <slave_1_ip>/" -i file || sed "$ a\slave1 <slave_1_ip>" -i /etc/hosts
-grep -q "^slave2" /etc/hosts && sed "s/^slave2=.*/slave1 <slave_2_ip>/" -i file || sed "$ a\slave2 <slave_2_ip>" -i /etc/hosts
-grep -q "^slave3" /etc/hosts && sed "s/^slave3=.*/slave1 <slave_3_ip>/" -i file || sed "$ a\slave3 <slave_3_ip>" -i /etc/hosts
+grep -q "^master1" /etc/hosts && sed "s/^master1=.*/master1 <master_1_ip>/" -i /etc/hosts || sed "$ a\master1 <master_1_ip>" -i /etc/hosts
+grep -q "^master2" /etc/hosts && sed "s/^master2=.*/master1 <master_2_ip>/" -i /etc/hosts || sed "$ a\master2 <master_2_ip>" -i /etc/hosts
+grep -q "^master3" /etc/hosts && sed "s/^master3=.*/master1 <master_3_ip>/" -i /etc/hosts || sed "$ a\master3 <master_3_ip>" -i /etc/hosts
+grep -q "^slave1" /etc/hosts && sed "s/^slave1=.*/slave1 <slave_1_ip>/" -i /etc/hosts || sed "$ a\slave1 <slave_1_ip>" -i /etc/hosts
+grep -q "^slave2" /etc/hosts && sed "s/^slave2=.*/slave1 <slave_2_ip>/" -i /etc/hosts || sed "$ a\slave2 <slave_2_ip>" -i /etc/hosts
+grep -q "^slave3" /etc/hosts && sed "s/^slave3=.*/slave1 <slave_3_ip>/" -i /etc/hosts || sed "$ a\slave3 <slave_3_ip>" -i /etc/hosts
 
 
 ###########################################################################################
 # Secondary Namenode  / Other services
 ###########################################################################################
-# Set up evrything mentioned in revious file..
-# rsync conf from namenode
+# Assumption is Hadoop is already configured ans setup(https://github.com/v8-suresh/hadoop_utils/blob/master/101_install_java_hadoop.sh)
+# To set the hostname
+cat master1 > /etc/hostname
+
+# Password less login from namenode to secondarynamenode
+
+# rsync conf from namenode to secondary namenode
+
 
 ###########################################################################################
 # data Nodes  / Yarn Task Nodes
