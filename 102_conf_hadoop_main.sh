57665656b3089c8917cd3dd0bd658d7c1a2e43c3
Suresh Sivanantham <suresh.sivanantham@gmail.com>
2014-08-29 22:24:36 +0530

    Update 102_conf_hadoop_main.sh

--- 102_conf_hadoop_main.sh
+++ 102_conf_hadoop_main.sh
@@ -19,7 +19,7 @@ chmod -R 744 .;
 #SEDing hadoop xml configs
 # core-site.xml
 #   fs.default.name
-sed -i '/<name>fs.default.name<\/name>/{n;s/.*/\t<value>hdfs:\/\/master1:8020<\/value>/;}' core-site.xml;   
+sed -i '/<name>fs.default.name<\/name>/{n;s/.*/\t<value>hdfs:\/\/<namenode_host_name>:8020<\/value>/;}' core-site.xml;   
 #   fs.checkpoint.dir   \
 sed -i '/<name>fs.checkpoint.dir<\/name>/{n;s/.*/\t<value>\/bigdata\/data\/fs.checkpoint<\/value>/;}' core-site.xml;        
 #   hadoop.tmp.dir  \
@@ -33,40 +33,40 @@ sed -i '/<name>dfs.name.dir<\/name>/{n;s/.*/\t<value>\/bigdata\/data\/dfs.name<\
 #   dfs.data.dir
 sed -i '/<name>dfs.data.dir<\/name>/{n;s/.*/\t<value>\/bigdata\/data\/dfs.data<\/value>/;}' hdfs-site.xml
 #   dfs.http.address
-sed -i '/<name>dfs.http.address<\/name>/{n;s/.*/\t<value>master2:50070<\/value>/;}' hdfs-site.xml
+sed -i '/<name>dfs.http.address<\/name>/{n;s/.*/\t<value><sec_namenode_host_name>:50070<\/value>/;}' hdfs-site.xml
 #   dfs.secondary.http.address
-sed -i '/<name>dfs.secondary.http.address<\/name>/{n;s/.*/\t<value>master2:50090<\/value>/;}' hdfs-site.xml
+sed -i '/<name>dfs.secondary.http.address<\/name>/{n;s/.*/\t<value><sec_namenode_host_name>:50090<\/value>/;}' hdfs-site.xml
 #   dfs.https.address
 sed -i '/<name>dfs.https.address<\/name>/{n;s/.*/\t<value>master2:50470<\/value>/;}' hdfs-site.xml
 
 # mapred-site.xml
 #   mapreduce.jobhistory.address
-sed -i '/<name>mapreduce.jobhistory.address<\/name>/{n;s/.*/\t<value>master2:10020<\/value>/;}' mapred-site.xml
+sed -i '/<name>mapreduce.jobhistory.address<\/name>/{n;s/.*/\t<value><sec_namenode_host_name>:10020<\/value>/;}' mapred-site.xml
 #   mapreduce.jobhistory.webapp.address
-sed -i '/<name>mapreduce.jobhistory.webapp.address<\/name>/{n;s/.*/\t<value>master2:19888<\/value>/;}' mapred-site.xml
+sed -i '/<name>mapreduce.jobhistory.webapp.address<\/name>/{n;s/.*/\t<value><sec_namenode_host_name>:19888<\/value>/;}' mapred-site.xml
 
     
 # yarn-site.xml
 #   yarn.nodemanager.local-dirs
 sed -i '/<name>yarn.nodemanager.local-dirs<\/name>/{n;s/.*/\t<value>\/bigdata\/data\/yarn\/local-dirs<\/value>/;}' yarn-site.xml
 #   yarn.resourcemanager.resource-tracker.address
-sed -i '/<name>yarn.resourcemanager.resource-tracker.address<\/name>/{n;s/.*/\t<value>master2:8025<\/value>/;}' yarn-site.xml
+sed -i '/<name>yarn.resourcemanager.resource-tracker.address<\/name>/{n;s/.*/\t<value><sec_namenode_host_name>:8025<\/value>/;}' yarn-site.xml
 #   yarn.resourcemanager.scheduler.address
-sed -i '/<name>yarn.resourcemanager.scheduler.address<\/name>/{n;s/.*/\t<value>master2:8030<\/value>/;}' yarn-site.xml
+sed -i '/<name>yarn.resourcemanager.scheduler.address<\/name>/{n;s/.*/\t<value><sec_namenode_host_name>:8030<\/value>/;}' yarn-site.xml
 #   yarn.resourcemanager.address
-sed -i '/<name>yarn.resourcemanager.address<\/name>/{n;s/.*/\t<value>master2:8050<\/value>/;}' yarn-site.xml
+sed -i '/<name>yarn.resourcemanager.address<\/name>/{n;s/.*/\t<value><sec_namenode_host_name>:8050<\/value>/;}' yarn-site.xml
 #   yarn.resourcemanager.admin.address
-sed -i '/<name>yarn.resourcemanager.admin.address<\/name>/{n;s/.*/\t<value>master2:8141<\/value>/;}' yarn-site.xml
+sed -i '/<name>yarn.resourcemanager.admin.address<\/name>/{n;s/.*/\t<value><sec_namenode_host_name>:8141<\/value>/;}' yarn-site.xml
 #   yarn.nodemanager.log-dirs
 sed -i '/<name>yarn.nodemanager.log-dirs<\/name>/{n;s/.*/\t<value>\/bigdata\/data\/yarn\/log-dirs<\/value>/;}' yarn-site.xml
 
 #   Login as root
-su root
-echo 'master1' > /etc/hostname
+#su root
+#echo "<sec_namenode_host_name>" > /etc/hostname
 
 
 # DNS for Namenode
-grep -q 'master1' /etc/hosts && sed -i "/master1/c\<ip>\tmaster1/" /etc/hosts || echo -e '<ip>\tmaster1' >> /etc/hosts
+#grep -q 'master1' /etc/hosts && sed -i "/master1/c\<ip>\tmaster1/" /etc/hosts || echo -e '<ip>\tmaster1' >> /etc/hosts
 # DNS for secondary namenode
 #grep -q 'master2' /etc/hosts && sed -i "/master2/c\<ip>\tmaster1/" /etc/hosts || echo -e '<ip>\tmaster2' >> /etc/hosts
 # DNS For Datanodes
