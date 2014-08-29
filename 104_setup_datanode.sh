8125cdfc986d4327658fc3946022390086dfd7d9
Suresh Sivanantham <suresh.sivanantham@gmail.com>
2014-08-29 20:33:45 +0530

    Create 104_setup_datanode.sh

---
+++ 104_setup_datanode.sh
@@ -0,0 +1,28 @@
+# Datanode node configuration
+# Assumption is Hadoop is already configured and setup in datanode machine
+#	  (https://github.com/v8-suresh/hadoop_utils/blob/master/100_setup_user_acc.sh)
+#     (https://github.com/v8-suresh/hadoop_utils/blob/master/101_install_java_hadoop.sh)
+# Login to namenode
+ssh -i <key_file_path> hadoop@<namenode_host_name>
+more $HADOOP_CONF_DIR/masters
+echo "<sec_namenode_host_name>" > $HADOOP_CONF_DIR/slaves
+more /etc/hosts
+su root
+grep -q '<datanode_host_name>' /etc/hosts && sed -i "/<datanode_host_name>/c\<datanode_ip>\t<datanode_host_name>/" /etc/hosts || echo -e '<sec_namenode_ip>\t<datanode_host_name>' >> /etc/hosts
+# For password less login from namenode to secondary namenode
+echo "Host <datanode_host_name>" >> ~/.ssh/config
+echo "IdentityFile ~/.ssh/<key_file_name>" >> ~/.ssh/config
+ssh <datanode_host_name>
+
+# rsync conf from namenode to secondary namenode
+rsync -avz -e "ssh" "$HADOOP_CONF_DIR/" "hadoop@<sec_namenode_host_name>:$HADOOP_CONF_DIR/"
+rsync -avz -e "ssh" "$HADOOP_CONF_DIR/" "hadoop@<datanode_host_name>:$HADOOP_CONF_DIR/"
+exit
+
+#	Login to Secondary namenode
+ssh -i <key_file_path> hadoop@<sec_namenode_host_name>
+su root
+grep -q '<namenode_host_name>' /etc/hosts && sed -i "/<namenode_host_name>/c\<namenode_ip>\t<sec_namenode_host_name>/" /etc/hosts || echo -e '<namenode_ip>\t<namenode_host_name>' >> /etc/hosts
+grep -q '<sec_namenode_host_name>' /etc/hosts && sed -i "/<sec_namenode_host_name>/c\<sec_namenode_ip>\t<sec_namenode_host_name>/" /etc/hosts || echo -e '<sec_namenode_ip>\t<sec_namenode_host_name>' >> /etc/hosts
+
+
