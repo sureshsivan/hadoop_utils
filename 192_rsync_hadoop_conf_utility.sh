318b1d0ced17d496ac87de9f72d6d791a2758a6c
Suresh Sivanantham <suresh.sivanantham@gmail.com>
2014-08-30 11:06:54 +0530

    Update 192_rsync_hadoop_conf_utility.sh

--- 192_rsync_hadoop_conf_utility.sh
+++ 192_rsync_hadoop_conf_utility.sh
@@ -1,2 +1,6 @@
 # To sync up hadoop configuration acroos all nodes in cluster
 # All nodes will be synced up from namenode hadoop configuration
+# Run this from namenode
+rsync -avz -e "ssh" "$HADOOP_CONF_DIR/" "hadoop@<sec_namenode_host_name>:$HADOOP_CONF_DIR/"
+# Run beow for every datanode
+  rsync -avz -e "ssh" "$HADOOP_CONF_DIR/" "hadoop@<datanode_host_name>:$HADOOP_CONF_DIR/"
