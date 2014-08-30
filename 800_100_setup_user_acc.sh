b3559b66417035f1ed150e46582daa98cfcef957
Suresh Sivanantham <suresh.sivanantham@gmail.com>
2014-08-30 13:22:19 +0530

    Removing tab between line breeaks

--- 800_100_setup_user_acc.sh
+++ 800_100_setup_user_acc.sh
@@ -11,11 +11,11 @@ sudo passwd;
 su root;
 # as [root]
 [4]	
-echo "<host_name>" > /etc/hostname;	\
-grep -q '<host_name>' /etc/hosts && sed -i "/<host_name>/c\<ip>\t<host_name>/" /etc/hosts || echo -e '<ip>\t<host_name>' >> /etc/hosts;	\
-mkdir -p /root/.ssh;	\
-touch authorized_keys;	\
-cat /home/ubuntu/.ssh/authorized_keys > /root/.ssh/authorized_keys;	\
+echo "<host_name>" > /etc/hostname;\
+grep -q '<host_name>' /etc/hosts && sed -i "/<host_name>/c\<ip>\t<host_name>/" /etc/hosts || echo -e '<ip>\t<host_name>' >> /etc/hosts;\
+mkdir -p /root/.ssh;\
+touch authorized_keys;\
+cat /home/ubuntu/.ssh/authorized_keys > /root/.ssh/authorized_keys;\
 chmod 600 /root/.ssh/authorized_keys;
 [5]
 exit;exit;
@@ -24,18 +24,18 @@ exit;exit;
 ssh -i <key_file_path> root@<ip_address>;
 # as [root]
 [7]
-groupadd hadoop;	\
+groupadd hadoop;\
 useradd -g hadoop -m hadoop -s /bin/bash;
 [8]
 passwd hadoop;
 [9]
-mkdir /home/hadoop/.ssh;	\
-touch /home/hadoop/.ssh/authorized_keys;	\
-cat /home/ubuntu/.ssh/authorized_keys > /home/hadoop/.ssh/authorized_keys;	\
-more /home/hadoop/.ssh/authorized_keys;	\
-ls -la /home/hadoop/.ssh;	\
-chown -R hadoop:hadoop /home/hadoop/.ssh;	\
-ls -la /home/hadoop/.ssh;	\
+mkdir /home/hadoop/.ssh;\
+touch /home/hadoop/.ssh/authorized_keys;\
+cat /home/ubuntu/.ssh/authorized_keys > /home/hadoop/.ssh/authorized_keys;\
+more /home/hadoop/.ssh/authorized_keys;\
+ls -la /home/hadoop/.ssh;\
+chown -R hadoop:hadoop /home/hadoop/.ssh;\
+ls -la /home/hadoop/.ssh;\
 [10]
 exit;
 
@@ -44,11 +44,11 @@ scp -i <key_file_path> <key_file_path> hadoop@<host_name>:/home/hadoop/.ssh/;
 [12]
 ssh -i <key_file_path> hadoop@<ip_address>;
 [13]
-touch ~/.ssh/config;	\
-chmod 600 ~/.ssh/config;	\
-echo "Host localhost" > ~/.ssh/config;	\
-echo "IdentityFile ~/.ssh/<key_file_name>" >> ~/.ssh/config;	\
-echo "Host <host_name>" >> ~/.ssh/config;	\
+touch ~/.ssh/config;\
+chmod 600 ~/.ssh/config;\
+echo "Host localhost" > ~/.ssh/config;\
+echo "IdentityFile ~/.ssh/<key_file_name>" >> ~/.ssh/config;\
+echo "Host <host_name>" >> ~/.ssh/config;\
 echo "IdentityFile ~/.ssh/<key_file_name>" >> ~/.ssh/config;
 
 [14]
