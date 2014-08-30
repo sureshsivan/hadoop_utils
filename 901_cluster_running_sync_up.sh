792a5eb09392c73d5b866b585a1f1d4ba137575d
Suresh Sivanantham <suresh.sivanantham@gmail.com>
2014-08-30 23:24:49 +0530

    Update 901_cluster_running_sync_up.sh

--- 901_cluster_running_sync_up.sh
+++ 901_cluster_running_sync_up.sh
@@ -6,15 +6,15 @@ hosts=("master1" "master2" "slave1" "slave2" "slave3");\
 ips=("1" "2" "3" "4" "5");\
 n=${#ips[*]};\
 for((i=0;i<n;i++));\
-do echo -e "${hosts[i]} ${ips[i]}" >> /etc/hosts;\
+do echo -e "${ips[i]} ${hosts[i]}" >> /etc/hosts;\
 done;
 
 # Find Private ip of all the hosts
-ssh -i HadoopEcoSystem.pem hadoop@master1 ifconfig eth0 | awk '/inet addr/{print substr($2,6)}'
-ssh -i HadoopEcoSystem.pem hadoop@master2 ifconfig eth0 | awk '/inet addr/{print substr($2,6)}'
-ssh -i HadoopEcoSystem.pem hadoop@slave1 ifconfig eth0 | awk '/inet addr/{print substr($2,6)}'
-ssh -i HadoopEcoSystem.pem hadoop@slave2 ifconfig eth0 | awk '/inet addr/{print substr($2,6)}'
-ssh -i HadoopEcoSystem.pem hadoop@slave3 ifconfig eth0 | awk '/inet addr/{print substr($2,6)}'
+ssh -i HadoopEcoSystem.pem root@master1 ifconfig eth0 | awk '/inet addr/{print substr($2,6)}';\
+ssh -i HadoopEcoSystem.pem root@master2 ifconfig eth0 | awk '/inet addr/{print substr($2,6)}';\
+ssh -i HadoopEcoSystem.pem root@slave1 ifconfig eth0 | awk '/inet addr/{print substr($2,6)}';\
+ssh -i HadoopEcoSystem.pem root@slave2 ifconfig eth0 | awk '/inet addr/{print substr($2,6)}';\
+ssh -i HadoopEcoSystem.pem root@slave3 ifconfig eth0 | awk '/inet addr/{print substr($2,6)}';
 
 # Do this after logging into every hosts
 sed -i '/HADOOP/q;p' /etc/hosts;\
@@ -22,7 +22,7 @@ hosts=("master1" "master2" "slave1" "slave2" "slave3");\
 ips=("1" "2" "3" "4" "5");\
 n=${#ips[*]};\
 for((i=0;i<n;i++));\
-do echo -e "${hosts[i]} ${ips[i]}" >> /etc/hosts;\
+do echo -e "${ips[i]} ${hosts[i]}" >> /etc/hosts;\
 done;
 
 
