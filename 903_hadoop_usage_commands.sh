2b661942d172b484812ee3290f030003918a2d7a
Suresh Sivanantham <suresh.sivanantham@gmail.com>
2014-08-31 21:13:20 +0530

    Update 903_hadoop_usage_commands.sh

--- 903_hadoop_usage_commands.sh
+++ 903_hadoop_usage_commands.sh
@@ -1,2 +1,25 @@
 [1]
 $HADOOP_PREFIX/bin/hdfs dfsadmin -report
+
+[2]
+$HADOOP_PREFIX/bin/hadoop fs -ls /
+$HADOOP_PREFIX/bin/hadoop fs -ls -R /
+$HADOOP_PREFIX/bin/hadoop fs -ls -R hdfs://master1:8020/
+
+[3]
+$HADOOP_PREFIX/bin/hadoop fs -mkdir hdfs://master1:8020/test1
+$HADOOP_PREFIX/bin/hadoop fs -mkdir /test2
+
+$HADOOP_PREFIX/bin/hadoop fs -mkdir hdfs://master1:8020/study/
+$HADOOP_PREFIX/bin/hadoop fs -mkdir hdfs://master1:8020/study/hadoopbook/
+$HADOOP_PREFIX/bin/hadoop fs -mkdir hdfs://master1:8020/study/hadoopbook/ncdc/
+$HADOOP_PREFIX/bin/hadoop fs -mkdir hdfs://master1:8020/study/hadoopbook/ncdc/all
+
+
+[4]
+
+
+$HADOOP_PREFIX/bin/
+$HADOOP_PREFIX/bin/
+$HADOOP_PREFIX/bin/
+$HADOOP_PREFIX/bin/
