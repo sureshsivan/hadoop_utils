bc926e9a4625a5c0e0e9b62d9fe56d71b640f4a8
Suresh Sivanantham <suresh.sivanantham@gmail.com>
2014-08-31 20:06:34 +0530

    Update 999_others.sh

--- 999_others.sh
+++ 999_others.sh
@@ -6,3 +6,22 @@ nano /etc/security/limits.conf
 [2]
 more $HADOOP_CONF_DIR/hdfs-site.xml
 
+[3]
+# DFS File System
+http://master1:50070/
+# File System - Browse
+http://master1:50070/explorer.html#/
+# secondary namenode status
+http://master2:50090/status.jsp
+#[IPC]	http://master2:10020/
+#Job History server
+http://master2:19888/jobhistory
+#[IPC]	http://master2:8025/
+#[IPC]	http://master2:8030/
+#[IPC]	http://master2:8050/
+#[IPC]	http://master2:8141/
+#[IPC]	http://slave1:45454/
+#[NG]	http://slave1:50010/
+#	Data Node status
+http://slave1:50075/dataNodeHome.jsp
+#[IPC]	http://slave1:8010/
